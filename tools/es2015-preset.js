481457a14b64ee88081724f3af057f61f762cfa7
Phebe Sawan <phebe.sawan@devpartners.co>
2020-01-24 09:07:24 +0800

    MILLI-659 fix es2015-preset.js

--- tools/es2015-preset.js
+++ tools/es2015-preset.js
@@ -1,8 +1,10 @@
 const BABEL_ENV = process.env.BABEL_ENV
 
-module.exports = () => ([
-  "@babel/preset-env", {
+module.exports = () => ({
+  presets: [
+    [ "@babel/preset-env", {
       loose: true,
       modules: BABEL_ENV === 'es' ? false : 'commonjs'
-    }
-])
+    } ]
+  ]
+})
